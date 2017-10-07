de0f3194d3db66a68d095eeb6f6938cc9cd59f50
alanrods <alanrods@outlook.com>
2017-10-06 19:19:53 -0500

    Centros y estaciones
    mapeo extremo

---
+++ practica-0303CIU/scripts/carga_datos/estacion.sql
@@ -0,0 +1,131 @@
+--CF1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(1,'CF1',st_GeomFromText('POINT(25.0125 11.9295)'));
+
+--C1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(2,'C1',st_GeomFromText('POINT(21	11)'));
+
+--C2
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(3,'C2',st_GeomFromText('POINT(17.2952 11.9506)'));
+
+--CA1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(4,'CA1',st_GeomFromText('POINT(14.5677 13.0505)'));
+
+--CM1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(5,'CM1',st_GeomFromText('POINT(12.0094 14.0024)'));
+
+--CR1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(6,'CR1',st_GeomFromText('POINT(8.01331 11.9295)'));
+
+--C3
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(7,'C3',st_GeomFromText('POINT(3.00235 11.9506)'));
+
+--C4
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(8,'C4',st_GeomFromText('POINT(1.98747 7.3396)'));
+
+--CF2
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(9,'CF2',st_GeomFromText('POINT(1.98747 3.9765)'));
+
+--AF1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(10,'AF1',st_GeomFromText('POINT(23.9554 18)'));
+
+--AF2
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(11,'AF2',st_GeomFromText('POINT(20.5301 14.4677)'));
+
+--A2
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(12,'A2',st_GeomFromText('POINT(16.0266 14.933)'));
+
+--AM1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(13,'AM1',st_GeomFromText('POINT(13.5106 11.5699)'));
+
+--RA1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(14,'RA1',st_GeomFromText('POINT(11.9248 9.98355)'));
+
+--A3
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(15,'A3',st_GeomFromText('POINT(9.00705 6.95887)'));
+
+--A4
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(16,'A4',st_GeomFromText('POINT(8.96476 3.00353)'));
+
+--AF3
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(17,'AF3',st_GeomFromText('POINT(4.14409 1.12103)'));
+
+--MF1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(18,'MF1',st_GeomFromText('POINT(11.0791 16.9636)'));
+
+--M2
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(19,'M2',st_GeomFromText('POINT(17.9295 9.64512)'));
+
+--M3
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(20,'M3',st_GeomFromText('POINT(20.0227 4.99177)'));
+
+--MF2
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(21,'MF2',st_GeomFromText('POINT(23.9765 4.94947)'));
+
+--RF1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(22,'RF1',st_GeomFromText('POINT(1.01488 17.8942)'));
+
+--R1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(23,'R1',st_GeomFromText('POINT(3.02349 14.9965)'));
+
+--R2
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(24,'R2',st_GeomFromText('POINT(6.02584 14.9753)'));
+
+--R3
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(25,'R3',st_GeomFromText('POINT(12.0094 5.05523)'));
+
+--RF2
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(26,'RF2',st_GeomFromText('POINT(16.0266 1.05758)'));
+
+--FF1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(27,'FF1',st_GeomFromText('POINT(15.9421 16.9636)'));
+
+--FFA1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(28,'FFA1',st_GeomFromText('POINT(27 11.9718)'));
+
+--FFA2
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(29,'FFA2',st_GeomFromText('POINT(27 15.0388)'));
+
+--FFB1
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(30,'FFB1',st_GeomFromText('POINT(11 1)'));
+
+--FFB2
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(31,'FFB2',st_GeomFromText('POINT(24.9702	0.994125)'));
+
+--FFB3
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(32,'FFB3',st_GeomFromText('POINT(26.9366 3.00353)'));
+
+--FFB4
+insert into ESTACION(ESTACION_ID, NOMBRE, GEO)
+values(33,'FFB4',st_GeomFromText('POINT(26.9789	4.92832)'));
