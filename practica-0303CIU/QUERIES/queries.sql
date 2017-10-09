d7316bc2a60f9da369940938ab5d9a64ee04e5cd
SilverAmadeus <ivanfenix_117@hotmail.com>
2017-10-09 13:33:18 -0500

    Query 2 para Open Jump e imagen de ejercicio 3

--- practica-0303CIU/QUERIES/queries.sql
+++ practica-0303CIU/QUERIES/queries.sql
@@ -19,3 +19,23 @@ UNION
 select av.nombre, 'area_verde' as tipo, st_distance(av.geo, st_boundary(q1.geo))
 from area_verde av, q1
 where st_dwithin(av.geo, st_boundary(q1.geo), 3);
+
+-- 2 Open Jump
+
+with q1 as(
+select st_buffer(e.geo, 1) as geo, e.nombre as ESTACION_NOMBRE from practica3.estacion e
+where e.nombre='AM1'
+    )
+select (ci.dump).geom, CENTRO_NOMBRE, ESTACION_NOMBRE
+from 
+(
+	select st_dump(geo) as dump, practica3.centro_interes.nombre AS CENTRO_NOMBRE from practica3.centro_interes
+	)ci, q1
+where st_dwithin((ci.dump).geom, st_boundary(q1.geo), 3)
+UNION
+select av.geo, av.nombre AS AV_NOMBRE, ESTACION_NOMBRE
+from practica3.area_verde av, q1
+where st_dwithin(av.geo, st_boundary(q1.geo), 3);
+
+-- 3
+
