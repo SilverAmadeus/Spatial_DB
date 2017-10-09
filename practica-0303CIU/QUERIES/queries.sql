07d13dd3e278d44fe2dd952d2d6703d7d7db2bba
SilverAmadeus <ivanfenix_117@hotmail.com>
2017-10-09 09:58:30 -0500

    Query 2 corregido
    No había tomado en cuenta que centro de interés es un ADT de tipo
    Multipoint, cambié a dump para poder usar cada punto del multipoint.
    
    en el st_within a pesar de regresa la distancia mínima entre dos
    objetos el Maestro comentó que es mejor asegurándose usando st_boundary
    para garantizar que está tomando la distancia máxima a partir del área
    alrededor de AM1.

--- practica-0303CIU/QUERIES/queries.sql
+++ practica-0303CIU/QUERIES/queries.sql
@@ -6,11 +6,16 @@ group by c.nombre;
 ---- 2
 -- Debe existir una union
 with q1 as(
-select st_buffer(e.geo, 1) as g1 from estacion e
+select st_buffer(e.geo, 1) as geo from estacion e
 where e.nombre='AM1'
     )
-select ci.nombre, ci.tipo as TIPO, st_distance(ci.geo, (q1.g1)) from centro_interes ci, q1
-where st_dwithin(ci.geo, (q1.g1), 3)
+select ci.nombre, ci.tipo as TIPO, st_distance((ci.dump).geom, st_boundary(q1.geo))
+from 
+(
+	select st_dump(geo) as dump , nombre, tipo from centro_interes
+	)ci, q1
+where st_dwithin((ci.dump).geom, st_boundary(q1.geo), 3)
 UNION
-select av.nombre, 'area_verde' as tipo, st_distance(av.geo, q1.g1) from area_verde av, q1
-where st_dwithin(av.geo, q1.g1, 3);
+select av.nombre, 'area_verde' as tipo, st_distance(av.geo, st_boundary(q1.geo))
+from area_verde av, q1
+where st_dwithin(av.geo, st_boundary(q1.geo), 3);
